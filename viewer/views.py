782d90fd2d67f119d6e854d601b7826b4ef18133
Nunu <bumbala8888@gmail.com>
2025-06-05 12:26:25 +0200

    Hello world.

--- viewer/views.py
+++ viewer/views.py
@@ -1,3 +1,37 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def hello(request):
+    # zde by byla funkcionalita
+    return HttpResponse("Hello, world!")
+
+
+def hello2(request, s):
+    # zde by byla funkcionalita
+    return HttpResponse(f"Hello, {s} world!")
+
+
+def hello3(request):
+    s = request.GET.get("s", "")
+    return HttpResponse(f"Hello, {s} world!")
+
+
+def hello4(request, s):
+    t = request.GET.get("t", "")
+    return HttpResponse(f"Your words: {s}, {t}")
+
+
+def add(request, num1, num2):
+    return HttpResponse(f"Adding {num1} + {num2} = {num1 + num2}")
+
+
+def add2(request):
+    num1 = int(request.GET.get("num1", ""))
+    num2 = int(request.GET.get("num2", ""))
+    return HttpResponse(f"Adding {num1} + {num2} = {num1 + num2}")
+
+
+
+
+
