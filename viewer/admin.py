376d9039a7f699441e4fa3968a4707fc6e4b1444
Nunu <bumbala8888@gmail.com>
2025-06-08 07:55:24 +0200

    Registered Movie model to admin.

--- viewer/admin.py
+++ viewer/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from viewer.models import Genre, Country, Creator
+from viewer.models import Genre, Country, Creator, Movie
 
 admin.site.register(Country)
 admin.site.register(Creator)
 admin.site.register(Genre)
+admin.site.register(Movie)
+
 
